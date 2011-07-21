4cedf471503720f109aa9e3f855e1d70759d91f7
Brian Corrigan <brian@agoragames.com>
2011-07-20 21:27:07 -0400

    Adding setup scripts

--- setup_symlinks.sh
+++ setup_symlinks.sh
@@ -1,2 +1,13 @@
-rm ~/.vimrc
-ln -s ./vimrc ~/.vimrc
+#!/bin/bash
+DIR=$(dirname $0)
+
+FILES=("vimrc" "zshrc" "oh-my-zsh") 
+
+for FILE in "${FILES[@]}"
+do
+  if [ -e "$FILE" ]
+  then
+    rm ~/.$FILE
+  fi
+  ln -s $DIR/$FILE ~/.$FILE
+done
