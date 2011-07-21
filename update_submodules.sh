5bf2f2b450d549278cbe374d141654cfb6dde382
Brian Corrigan <brian@agoragames.com>
2011-07-20 21:27:43 -0400

    Adding setup scripts

---
+++ update_submodules.sh
@@ -0,0 +1,4 @@
+git submodule update --init vim/bundle/fugitive
+git submodule update --init vim/bundle/vim-colors-solarized
+git submodule update --init oh-my-zsh
+git submodule foreach git pull origin master
